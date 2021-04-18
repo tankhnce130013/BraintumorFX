234fe6479b77b96c7baecb8ca4594ba971a78237
tankhnce130013 <52484292+tankhnce130013@users.noreply.github.com>
2021-04-19 01:55:01 +0700

    create FAT JAR Include all dependencies to single jar

--- Braintumor/src/main/java/module-info.java
+++ Braintumor/src/main/java/module-info.java
@@ -5,11 +5,10 @@ module org.fpt {
     requires org.json;
     requires javafx.web;
     requires javafx.swing;
-    requires okhttp3;
     requires de.jensd.fx.glyphs.commons;
     requires de.jensd.fx.glyphs.materialdesignicons;
     requires de.jensd.fx.glyphs.fontawesome;
-    requires javafx.graphicsEmpty;
+    requires okhttp3;
 
     opens org.fpt to javafx.fxml;
     opens org.fpt.login to javafx.fxml;
